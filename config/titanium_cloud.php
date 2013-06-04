4efa446f99430fa46e20e02f80ad61355a733f84
Joel Herron <herronj@gmail.com>
2013-06-04 16:09:48 -0500

    removed wrong license comment

--- config/titanium_cloud.php
+++ config/titanium_cloud.php
@@ -1,26 +1,9 @@
 $<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/*
- * This file is part of Titanium_Cloud.
-
-    Titanium_Cloud is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Titanium_Cloud is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Titanium Cloud.  If not, see <http://www.gnu.org/licenses/>.
- * 
- */
 
 /**
  * @author      Joel Herron <@h3r2on>
  * @copyright   Copyright © 2013 by Joel Herron <@h3r2on>
- * @package     TitaniumC_loud
+ * @package     Titanium_Cloud
  * @subpackage  configuration
  * @license     MIT License http://www.opensource.org/licenses/mit-license.php
  */
